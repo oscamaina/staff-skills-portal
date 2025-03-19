69bb2d39ef1af0197ad4063c1a1633b279b0f105
oscar <oscar.maina@vodafone.com>
2025-03-19 12:40:28 +0300

    removed lombok, added missing posgresql missing configuration

--- backend/src/main/java/com/portal/model/Staff.java
+++ backend/src/main/java/com/portal/model/Staff.java
@@ -1,11 +1,10 @@
 package com.portal.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
 public class Staff {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,4 +19,120 @@ public class Staff {
     private String softwareExpertiseLevel;
     private String language;
     private String levelOfResponsibility;
+
+    public Staff() {}
+
+    public Staff(Long id, String indexNumber, String fullName, String email, String currentLocation, 
+                 String highestLevelOfEducation, String dutyStation, String availabilityForRemoteWork, 
+                 String softwareExpertise, String softwareExpertiseLevel, String language, 
+                 String levelOfResponsibility) {
+        this.id = id;
+        this.indexNumber = indexNumber;
+        this.fullName = fullName;
+        this.email = email;
+        this.currentLocation = currentLocation;
+        this.highestLevelOfEducation = highestLevelOfEducation;
+        this.dutyStation = dutyStation;
+        this.availabilityForRemoteWork = availabilityForRemoteWork;
+        this.softwareExpertise = softwareExpertise;
+        this.softwareExpertiseLevel = softwareExpertiseLevel;
+        this.language = language;
+        this.levelOfResponsibility = levelOfResponsibility;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIndexNumber() {
+        return indexNumber;
+    }
+
+    public void setIndexNumber(String indexNumber) {
+        this.indexNumber = indexNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(String currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public String getHighestLevelOfEducation() {
+        return highestLevelOfEducation;
+    }
+
+    public void setHighestLevelOfEducation(String highestLevelOfEducation) {
+        this.highestLevelOfEducation = highestLevelOfEducation;
+    }
+
+    public String getDutyStation() {
+        return dutyStation;
+    }
+
+    public void setDutyStation(String dutyStation) {
+        this.dutyStation = dutyStation;
+    }
+
+    public String getAvailabilityForRemoteWork() {
+        return availabilityForRemoteWork;
+    }
+
+    public void setAvailabilityForRemoteWork(String availabilityForRemoteWork) {
+        this.availabilityForRemoteWork = availabilityForRemoteWork;
+    }
+
+    public String getSoftwareExpertise() {
+        return softwareExpertise;
+    }
+
+    public void setSoftwareExpertise(String softwareExpertise) {
+        this.softwareExpertise = softwareExpertise;
+    }
+
+    public String getSoftwareExpertiseLevel() {
+        return softwareExpertiseLevel;
+    }
+
+    public void setSoftwareExpertiseLevel(String softwareExpertiseLevel) {
+        this.softwareExpertiseLevel = softwareExpertiseLevel;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getLevelOfResponsibility() {
+        return levelOfResponsibility;
+    }
+
+    public void setLevelOfResponsibility(String levelOfResponsibility) {
+        this.levelOfResponsibility = levelOfResponsibility;
+    }
 }
